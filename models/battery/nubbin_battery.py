b57510e41237409384f660775da35b57d5ad3bce
Uchenna Oko <uche4newton1981@gmail.com>
2023-10-29 14:34:22 +0100

    Updating Models Package

--- models/battery/nubbin_battery.py
+++ models/battery/nubbin_battery.py
@@ -5,11 +5,11 @@ from models.utils import add_years_to_date
 
 
 class NubbinBattery(Battery):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date
+    def __init__(self, current_date, last_service_date):
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
-    def need_service(self):
+    def needs_service(self):
         date_battery_be_serviced = add_years_to_date(self.last_service_date, 4)
         if date_battery_be_serviced < self.current_date:
             return True
--- models/battery/spindler_battery.py
+++ models/battery/spindler_battery.py
@@ -5,9 +5,9 @@ from models.utils import add_years_to_date
 
 
 class SpindlerBattery(Battery):
-    def __init__(self, last_service_date, current_date):
-        self.last_service_date = last_service_date        
+    def __init__(self, current_date, last_service_date):     
         self.current_date = current_date
+        self.last_service_date = last_service_date
 
     def needs_service(self):
         date_battery_be_serviced = add_years_to_date(self.last_service_date,  2)
