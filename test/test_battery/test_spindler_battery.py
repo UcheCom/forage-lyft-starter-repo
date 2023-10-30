013bda8377eb109b7314e246c613baa7cb11c3f3
Uchenna Oko <uche4newton1981@gmail.com>
2023-10-30 01:54:26 +0100

    Updating Test Package

--- test/test_battery/test_spindler_battery.py
+++ test/test_battery/test_spindler_battery.py
@@ -8,13 +8,13 @@ from models.battery.spindler_battery import SpindlerBattery
 
 class TestSpindlerBattery(unittest.TestCase):
     def test_needs_service(self):
-        current_date = date.fromisoformat("2023-10-28")
-        last_service_date = date.fromisoformat("2021-06-30")
+        current_date = date.fromisoformat("2023-10-29")
+        last_service_date = date.fromisoformat("2020-06-30")
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertTrue(battery.needs_service())
 
     def test_needs_not_service(self):
-        current_date = date.fromisoformat("2023-10-28")
+        current_date = date.fromisoformat("2023-10-29")
         last_service_date = date.fromisoformat("2022-06-15")
         battery = SpindlerBattery(current_date, last_service_date)
         self.assertFalse(battery.needs_service())
